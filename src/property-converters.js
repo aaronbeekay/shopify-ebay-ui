d7b1473e55247e243dc931566c4f1895e60406cd
Glitch (starter-react-template) <none>
2019-05-04 20:36:09 +0000

    🚔🕴 Checkpoint ./src/property-converters.js:4253064/1009

--- src/property-converters.js
+++ src/property-converters.js
@@ -1,13 +1,36 @@
 function convert_shopify_property(key, product){
  switch(key){
    case 'title':
-     return shopify_title_to_ebay_title(product.title);
+     return shopify_title_to_ebay_title(product);
      break;
    case 'weight':
+     return shopify_weight_to_ebay_weight(product);
+     break;
+   default:
+     return '???'
      
  }
 }
 
-function shopify_title_to_ebay_title(title){
-  return title;
+function shopify_title_to_ebay_title(product){
+  return {"product": {"title": product.title}};
+}
+
+function shopify_weight_to_ebay_weight(product){
+  /* For whatever reason, product weight is stored only in product variants ...?
+   * right now, just return the weight of the first variant :(
+   */
+  var shopify_weight = product.variants[0].grams;
+  var ebay_weight = {
+    "packageWeightAndSize": {
+      "weight": {
+        "unit" : "GRAM",
+        "value": shopify_weight
+        }
+      }
+    };
+}
+
+function shopify_condition_to_ebay_condition(product){
+  // We store the condition in the first Shopify optio
 }
